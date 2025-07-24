534afefdfe3348a8fe9a4c74bae936f6e22820e1
Piyush Maurya <PiyushMaurya.cs@gmail.com>
2025-07-25 01:37:03 +0530

    Learn react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Maurya JI, Kaise ho!</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Chai Peelo JI! || MauryaJI</h1>
+  )
+}
+
+export default App
